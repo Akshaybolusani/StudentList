e56f5e27f29d23d23476d3594d8a9e4a3eef0ebc
Akshaybolusani <115645505+Akshaybolusani@users.noreply.github.com>
2025-02-06 10:23:02 +0530

    Create 6thFeb.js

---
+++ 6thFeb.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+
+const Welcome=()=>{
+    return(
+        <div>
+            <h1 className='mess'>Welcome to React</h1>
+        </div>
+    )
+}
+
+function App(){
+  return <Welcome/>
+}
+
+export default App;
